30b221d627d0adae2122d0b4f96af14ef3d664ad
pigulevsky gleb <gleb.pigulevsky@hqo.co>
2021-09-19 21:01:37 +0300

    Initial Commit

---
+++ Shared/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  Shared
+//
+//  Created by MacBookPro on 19.09.21.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
